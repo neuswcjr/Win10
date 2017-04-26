935ad335d7e748d71c621e6025e0da764b89e521
unknown <>
2017-04-26 18:24:30 +0800

    添加项目文件。

---
+++ App3/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace App3
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+		private void OnItemClick(object sender, ItemClickEventArgs e)
+		{
+			//frame.Navigate()
+			string item = e.ClickedItem as string;
+			if (!string.IsNullOrEmpty(item))
+			{
+				switch (item)
+				{
+					case "Page 1":
+						frame.Navigate(typeof(Page1));
+						break;
+					case "Page 2":
+						frame.Navigate(typeof(Page2));
+						break;
+					case "Page 3":
+						frame.Navigate(typeof(Page3));
+						break;
+				}
+			}
+		}
+
+		private void OnFrameNavigating(object sender, NavigatingCancelEventArgs e)
+		{
+			string msg = "Frame navigating, SourcePageType = {0}";
+			System.Diagnostics.Debug.WriteLine(msg, e.SourcePageType);
+		}
+
+		private void OnFrameNavigated(object sender, NavigationEventArgs e)
+		{
+			string msg = "Frame Navigated, SourcePageType = {0}";
+			System.Diagnostics.Debug.WriteLine(msg, e.SourcePageType);
+		}
+	}
+}
---
+++ App3/Page2.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace App3
+{
+	/// <summary>
+	/// 可用于自身或导航至 Frame 内部的空白页。
+	/// </summary>
+	public sealed partial class Page2 : Page
+	{
+		public Page2()
+		{
+			this.InitializeComponent();
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			Debug.WriteLine("Page2:OnNavigatedTo call");
+		}
+
+		protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+		{
+			Debug.WriteLine("Page2:OnNavigatingFrom call");
+		}
+
+		protected override void OnNavigatedFrom(NavigationEventArgs e)
+		{
+			Debug.WriteLine("Page2:OnNavigatedFrom call");
+		}
+	}
+}
