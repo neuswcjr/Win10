29407d21d47d2807dd3a4c2772e8ebee4b46c4d1
Ruo Jia <neuswcjr@outlook.com>
2017-04-27 01:06:32 +0800

    section3.3 demo

---
+++ App2/Page_A.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace App2
+{
+	/// <summary>
+	/// 可用于自身或导航至 Frame 内部的空白页。
+	/// </summary>
+	public sealed partial class Page_A : Page
+	{
+		public Page_A()
+		{
+			this.InitializeComponent();
+		}
+
+		private void OnBack(object sender, RoutedEventArgs e)
+		{
+			if (this.Frame.CanGoBack)
+			{
+				this.Frame.GoBack();
+			}
+		}
+
+		private void OnForward(object sender, RoutedEventArgs e)
+		{
+			if (this.Frame.CanGoForward)
+			{
+				this.Frame.GoForward();
+			}
+			else
+			{
+				this.Frame.Navigate(typeof(Page_B));
+			}
+		}
+
+		protected override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			this.tbInfo.Text = "导航模式： " + e.NavigationMode.ToString();
+		}
+	}
+}
---
+++ App2/PopupControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace App2
+{
+	public sealed partial class PopupControl : UserControl
+	{
+		Frame m_frame = null;
+		public PopupControl( Frame frame)
+		{
+			this.InitializeComponent();
+			m_frame = frame;
+			this.Loaded += (s1, a1) =>
+			{
+				m_frame.Navigated += M_frame_Navigated;
+			};
+			this.Unloaded += (s1, a2) =>
+			{
+				m_frame.Navigated += M_frame_Navigated;
+			};
+		}
+
+		void M_frame_Navigated(object sender, NavigationEventArgs e)
+		{
+			lvBack.Items.Clear();
+			foreach (PageStackEntry item in m_frame.BackStack)
+			{
+				lvBack.Items.Add(item.SourcePageType.Name);
+			}
+			lvForward.Items.Clear();
+			foreach (PageStackEntry item in m_frame.ForwardStack)
+			{
+				lvForward.Items.Add(item.SourcePageType.Name);
+			}
+		}
+	}
+}
